08b1a24f15cc9fa11b59e9bed7e16dbfd38b2362
eternity-phoenix <eternity_phoenix@outlook.com>
2015-11-27 22:42:46 +0800

    some modified
    but i'm confused. web is a terrible and boring process.
    i hope to finish this fast.

--- www/handlers.py
+++ www/handlers.py
@@ -27,7 +27,18 @@ from aiohttp import web
 
 @get(['/', '/index', '/home'])
 def index(request) :
-    return web.Response(body = b'<h1>Awesome</h1>', content_type = "text/html")
+    #return web.Response(body = b'<h1>Awesome</h1>', content_type = "text/html")
+    summary = r'''the quick brown fox jumps over a lazy dog.
+    i'm live for study and codes...'''
+    blogs = [
+        Blog(id = 1, name = 'Test Blog1', summary = summary, created_at = time.time() - 220)
+        Blog(id = 2, name = 'Eternity_Phoenix', summary = summary, created_at = time.time() - 1220)
+        Blog(id = 3, name = 'lone', summary = summary, created_at = time.time() - 2220)
+    ]
+    return {
+        '__template__' : 'blogs.html'
+        'blogs' : blogs
+    }
 
 @get('/test')
 def test(request) :
