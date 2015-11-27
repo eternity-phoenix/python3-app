f4b73cb3c183ec267e6195d13c85ae0e9ef055ca
eternity-phoenix <eternity_phoenix@outlook.com>
2015-11-27 11:55:06 +0800

    coroweb continue

--- www/app.py
+++ www/app.py
@@ -14,8 +14,63 @@ import asyncio, os, json, time, socket
 from datetime import datetime
 
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
 
+from coroweb import add_routes, add_static
 import orm
+
+
+'''
+middleware
+
+middleware是一种拦截器，一个URL在被某个函数处理前，可以经过一系列的middleware的处理。
+
+一个middleware可以改变URL的输入、输出，甚至可以决定不继续处理而直接返回。
+middleware的用处就在于把通用的功能从每个URL处理函数中拿出来，集中放到一个地方。例如，
+一个记录URL日志的logger可以简单定义如下：
+
+@asyncio.coroutine
+def logger_factory(app, handler):
+    @asyncio.coroutine
+    def logger(request):
+        # 记录日志:
+        logging.info('Request: %s %s' % (request.method, request.path))
+        # 继续处理请求:
+        return (yield from handler(request))
+    return logger
+而response这个middleware把返回值转换为web.Response对象再返回，以保证满足aiohttp的要求：
+
+@asyncio.coroutine
+def response_factory(app, handler):
+    @asyncio.coroutine
+    def response(request):
+        # 结果:
+        r = yield from handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            ...
+有了这些基础设施，我们就可以专注地往handlers模块不断添加URL处理函数了，可以极大地提高开发效率。
+'''
+def init_jinja2(app, **kw) :
+    logging.info('init jinja2...')
+    options = dict(
+        autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+
 
 def index(request) :
     return web.Response(body = b'<h1>Awesome</h1>', content_type = "text/html")
--- www/coroweb.py
+++ www/coroweb.py
@@ -161,10 +161,104 @@ class RequestHandler(object) :
     def __str__(self) :
         return str(self.__dict__)
 
-    @asyncio.coroutine
+    #@asyncio.coroutine
     def __call__(self, request) :
         kw = None
-        if self._has_var_kw_args or
+        if self._has_var_kw_args or self._has_named_kw_args or self._has_request_arg :
+            if request.method == 'POST' :
+                if not request.content_type :
+                    return web.HTTPBadRequest('Missing body Content-Type.')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json') :
+                    params = yield from request.json()
+                    if not isinstance(params, dict) :
+                        return web.HTTPBadRequest('JSON body must be object')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data') :
+                    params = yield from request.post()
+                    kw = dict(**params)
+                else :
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET' :
+                qs = request.query_string
+                if qs :
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items() :
+                        kw[k] = v[0]
+        if kw is None :
+            kw = dict(**request.match_info)
+        else :
+            if not self._has_var_kw_args and self._named_kw_args :
+                #remove all unamed kw:
+                copy = dict()
+                for name in self._named_kw_args :
+                    if name in kw :
+                        copy[name] = kw[name]
+                kw = copy
+            #check named arg:
+            for k, v in request.match_info.items() :
+                if k in kw :
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg :
+            kw['request'] = request
+            #check required kw:
+            if self._required_kw_args :
+                for name in self._required_kw_args :
+                    if not name in kw :
+                        return web.HTTPBadRequest('Missing argument: %s' % name)
+            logging.info('call with args: %s' % str(kw))
+            try :
+                r = yield from self._func(**kw)
+                return r
+            except APIError as e :
+                logging.warning('found a error! at line 215 of coroweb')
+                return dict(error = e.error, data = e.data, message = e.message)
+'''
+再编写一个add_route函数，用来注册一个URL处理函数：
+最后一步，把很多次add_route()注册的调用：
+add_route(app, handles.index)
+add_route(app, handles.blog)
+add_route(app, handles.create_comment)
+...
+变成自动扫描：
+
+# 自动把handler模块的所有符合条件的函数注册了:
+add_routes(app, 'handlers')
+add_routes()定义如下：
+后，在app.py中加入middleware、jinja2模板和自注册的支持：
+'''
+def add_static(app) :
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s!' % ('/static/', path))
+
+def add_route(app, fn) :
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None :
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn) :
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name) :
+    n = module_name.rfind('.')
+    if n == -1 :
+        mod = __import__(module_name, globals(), locals())
+    else :
+        name = module_name[n + 1 :]
+        mod = getattr(__import__(module_name[: n], globals(), locals(), [name]), name)
+    for attr in dir(mod) :
+        if attr.startswith('_') :
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn) :
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path :
+                add_route(app, fn)
 
 if __name__ == '__main__' :
     def test(a, b, c = 0, *args, **kw) :
@@ -175,7 +269,13 @@ if __name__ == '__main__' :
         pass
     def test2(a, request, aa, **kw) :
         pass
-    print(RequestHandler('111', test))
+    x = RequestHandler('111', test)
+    print(x)
     print(RequestHandler('111', te))
     print(RequestHandler('111', test1))
     #print(RequestHandler('111', test2))
+    '''print()
+    from urllib.request import Request
+    z = Request(url = 'http://www.baidu.com', method = 'GET')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(x(z))'''
